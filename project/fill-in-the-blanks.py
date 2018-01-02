90cf53ce81bec83fd8ab9035e81571f410737ba0
ZacharyCWilliams <34009260+ZacharyCWilliams@users.noreply.github.com>
2018-01-02 10:19:17 -0800

    Update fill-in-the-blanks.py

--- project/fill-in-the-blanks.py
+++ project/fill-in-the-blanks.py
@@ -18,111 +18,60 @@ game_data = {
     }
 }
 
-def game_play():
-    #pulling number of guesses allowed from game_data structure
-    number_of_guesses = game_data[select_level]['number_of_guesses']
-    #asking user for their first answer
-    first_question = raw_input('\nWhat should be substituted for __1__? ')
-    #pulling correct answer from data structure
-    first_answer = game_data[select_level]['answers'][0]
-    #as long as they guess incorrectly, let them know they're wrong, reduce number of guesses by 1, and ask again
-    while first_question != first_answer:
-        if number_of_guesses > 2:
+
+quiz = None
+answers = None
+start_index = None
+number_of_guesses = None
+user_input = None
+
+def incorrect_guess():
+    if (user_input == answer):
+        game_play()
+    while (user_input != answer):
+        if (number_of_guesses > 2):
             number_of_guesses = number_of_guesses - 1
             print '\nOops! That\'s not right. You have ' + str(number_of_guesses) + ' guesses left! Try again:'
-            first_question = raw_input('\nWhat should be substituted for __1__? ')
-        elif number_of_guesses == 2:
+            user_input = raw_input('What should be substituted for __' + str(index) + '__?')
+        elif (number_of_guesses == 2):
             number_of_guesses = number_of_guesses - 1
             print '\nOops! That\'s not right. This is your LAST guess: make it count!'
-            first_question = raw_input('\nWhat should be substituted for __1__? ')
-        else:
-            #if number_of_guesses == 0 reset game
-            print '\nYou Lose! Let\'s play again!'
-            on_load()
-            
-    #if they're correct print the new version of the quiz with first answer filled in!
-    if first_question == first_answer:
-        replace_quiz = game_data[select_level]['quiz']
-        replace_quiz = replace_quiz.replace('__1__', first_answer)
-        print '\nThat\'s right!'
-        print replace_quiz
-
-        #moving on to Question 2
-        second_question = raw_input('\nWhat should be substituted for __2__? ')
-        #setting second answer (pulling from data set)
-        second_answer = game_data[select_level]['answers'][1]
-        #resetting number of guesses
-        number_of_guesses = 5
-
-        while second_question != second_answer:
-            if number_of_guesses > 2:
-                #as long as they guess incorrectly, let them know they're wrong, reduce number of guesses by 1, and ask again
-                number_of_guesses = number_of_guesses - 1
-                print '\nOops! That\'s not right. You have ' + str(number_of_guesses) + ' guesses left! Try again:'
-                second_question = raw_input('\nWhat should be substituted for __2__? ')
-            elif number_of_guesses == 2:
-                number_of_guesses = number_of_guesses - 1
-                print '\nOops! That\'s not right. This is your LAST guess: make it count!'
-                second_question = raw_input('\nWhat should be substituted for __2__? ')
-            else:
-                print '\nYou Lose! Let\'s play again!'
+            user_input = raw_input('What should be substituted for __' + str(index) + '__?')
+        elif (number_of_guesses == 1):
+                print '\nYou Lose! Let\'s play again!\n'
                 on_load()
 
-        if second_question == second_answer:
-            replace_quiz = replace_quiz.replace('__2__', second_answer)
-            print '\nThat\'s right!'
-            print replace_quiz
 
-            third_question = raw_input('\nWhat should be substituted for __3__? ')
-            third_answer = game_data[select_level]['answers'][2]
-            number_of_guesses = 5
+def game_play():
+    global quiz
+    quiz = game_data[select_level]['quiz']
+    global answers
+    answers = game_data[select_level]['answers']
+    global start_index
+    start_index = 1
+    global number_of_guesses
+    number_of_guesses = game_data[select_level]['number_of_guesses']
 
-            while third_question != third_answer:
-                if number_of_guesses > 2:
-                    #as long as they guess incorrectly, let them know they're wrong, reduce number of guesses by 1, and ask again
-                    number_of_guesses = number_of_guesses - 1
-                    print '\nOops! That\'s not right. You have ' + str(number_of_guesses) + ' guesses left! Try again:'
-                    third_question = raw_input('\nWhat should be substituted for __3__? ')
-                elif number_of_guesses == 2:
-                    number_of_guesses = number_of_guesses - 1
-                    print '\nOops! That\'s not right. This is your LAST guess: make it count!'
-                    third_question = raw_input('\nWhat should be substituted for __3__? ')
-                else:
-                    print '\nYou Lose! Let\'s play again!'
-                    on_load()
-
-            if third_question == third_answer:
-                replace_quiz = replace_quiz.replace('__3__', third_answer)
+    for index, answer in enumerate(answers, start_index):
+        while True:
+            global user_input
+            user_input = raw_input('\nWhat should be substituted for __' + str(index) + '__? ')
+            if (user_input == answer):
                 print '\nThat\'s right!'
-                print replace_quiz
-
-                fourth_question = raw_input('\nWhat should be substituted for __4__? ')
-                fourth_answer = game_data[select_level]['answers'][3]
-                number_of_guesses = 5
-
-                while fourth_question != fourth_answer:
-                    if number_of_guesses > 2:
-                        #as long as they guess incorrectly, let them know they're wrong, reduce number of guesses by 1, and ask again
-                        number_of_guesses = number_of_guesses - 1
-                        print '\nOops! That\'s not right. You have ' + str(number_of_guesses) + ' guesses left! Try again:'
-                        fourth_question = raw_input('\nWhat should be substituted for __4__? ')
-                    elif number_of_guesses == 2:
-                        number_of_guesses = number_of_guesses - 1
-                        print '\nOops! That\'s not right. This is your LAST guess: make it count!'
-                        fourth_question = raw_input('\nWhat should be substituted for __4__? ')
-                    else:
-                        print '\nYou Lose! Let\'s play again!'
-                        on_load()
-
-                if fourth_question == fourth_answer:
-                    #if they're correct print the new version of the quiz with first answer filled in!
-                    replace_quiz = replace_quiz.replace('__4__', fourth_answer)
-                    print '\nYay! You Win! The final version is:'
-                    #print finalized version of quiz
-                    print replace_quiz
-                    print '\n'
-                    #when user wins restart game!
+                quiz = quiz.replace('__' + str(index) + '__', answer)
+                print quiz
+                index = index + 1
+                if index <= 4:
+                    answer = answers[index- 1]
+                if index == 5:
+                    quiz = quiz.replace('__' + str(index) + '__', answer)
+                    print '\nYay! You won! The final solution is:'
+                    print quiz
+                    print ''
                     on_load()
+            elif (user_input != answer):
+                incorrect_guess()
+
 
 #set global variable to None so that all functions can access variable && we can set/reset variable in local scopes
 select_level = None
